25536b9642338ae472719cdb63d3daf7f0164550
suchonUL <mikolajsuchon2@gmail.com>
2023-06-14 18:24:19 +0200

    .

---
+++ Lab9/my_rand_ex.hpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+#include<vector>
+
+
+int my_rand(int,int);
+
+
+
+void my_rand(int* randomNumberVector,int rows,int minNumber,int maxNumber);
+
+
+
+void my_rand(std::vector<std::vector<int>>* randomNumberMatrix, int rows, int columns, int minNumber, int maxNumber);
